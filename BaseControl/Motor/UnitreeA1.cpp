5f4dc842169a9013130652340349a7f7febc6afd
Haoyu Chen <null-qwerty@outlook.com>
2025-03-19 16:34:15 +0800

    feat(Motor): interface of Unitree A1 motor

---
+++ BaseControl/Motor/UnitreeA1.cpp
@@ -0,0 +1,159 @@
+#include "BaseControl/Motor/UnitreeA1.hpp"
+#include "Math/Math.hpp"
+#include <string.h>
+
+UnitreeA1::UnitreeA1(Connectivity &connectivity, uint16_t send_id,
+                     uint16_t receive_id, int8_t cw)
+    : Motor(connectivity, send_id, receive_id)
+{
+    this->clockwise *= cw;
+}
+
+UnitreeA1::~UnitreeA1()
+{
+}
+
+UnitreeA1 &UnitreeA1::init()
+{
+    refState.position = 0;
+    refState.velocity = 0;
+    refState.toreque = 0;
+    refState.temprature = 0;
+
+    state.position = 0;
+    state.velocity = 0;
+    state.toreque = 0;
+    state.temprature = 0;
+
+    auto readIndex =
+        ((UART::xUARTFrame_t *)(connectivity.getSendFrame()))->readIndex;
+
+    sendData *sendBuffer =
+        (sendData *)(((UART::xUARTFrame_t *)(connectivity.getSendFrame()))
+                         ->data[1 - readIndex]);
+
+    memset(sendBuffer, 0, sizeof(sendData));
+
+    sendBuffer->header.start = 0xeefe;
+    sendBuffer->header.id = send_id;
+    sendBuffer->data.mode = 10;
+    sendBuffer->crc = crc32_core((uint32_t *)(sendBuffer), 7);
+
+    ifEnable = 1;
+
+    return *this;
+}
+
+UnitreeA1 &UnitreeA1::deInit()
+{
+    ifEnable = 0;
+
+    auto readIndex =
+        ((UART::xUARTFrame_t *)(connectivity.getSendFrame()))->readIndex;
+
+    sendData *sendBuffer =
+        (sendData *)(((UART::xUARTFrame_t *)(connectivity.getSendFrame()))
+                         ->data[1 - readIndex]);
+
+    memset(sendBuffer, 0, sizeof(sendData));
+
+    sendBuffer->header.start = 0xeefe;
+    sendBuffer->header.id = send_id;
+    sendBuffer->data.mode = 0;
+    sendBuffer->crc = crc32_core((uint32_t *)(sendBuffer), 7);
+
+    return *this;
+}
+
+UnitreeA1 &UnitreeA1::encodeControlMessage()
+{
+    int16_t data = calculateControlData();
+
+    auto readIndex =
+        ((UART::xUARTFrame_t *)(connectivity.getSendFrame()))->readIndex;
+
+    sendData *sendBuffer =
+        (sendData *)(((UART::xUARTFrame_t *)(connectivity.getSendFrame()))
+                         ->data[1 - readIndex]);
+
+    memset(sendBuffer, 0, sizeof(sendData));
+
+    sendBuffer->header.start = 0xeefe;
+    sendBuffer->header.id = send_id;
+    sendBuffer->data.mode = 10;
+    sendBuffer->data.T = data * 256;
+    sendBuffer->crc = crc32_core((uint32_t *)(sendBuffer), 7);
+
+    return *this;
+}
+
+UnitreeA1 &UnitreeA1::decodeFeedbackMessage()
+{
+    auto readIndex =
+        ((UART::xUARTFrame_t *)(connectivity.getReceiveFrame()))->readIndex;
+
+    receiveData *receiveBuffer =
+        (receiveData *)(((UART::xUARTFrame_t *)(connectivity.getReceiveFrame()))
+                            ->data[readIndex]);
+
+    if (receiveBuffer->header.start == 0xeefe &&
+        receiveBuffer->header.id == receive_id &&
+        receiveBuffer->crc == crc32_core((uint32_t *)(receiveBuffer), 9)) {
+        state.position = receiveBuffer->data.Pos * 2 * PI / 16384.;
+        state.velocity = receiveBuffer->data.W / 128.;
+        state.toreque = receiveBuffer->data.T / 256.;
+        state.temprature = receiveBuffer->data.Temp;
+    }
+
+    return *this;
+}
+
+float UnitreeA1::calculateControlData()
+{
+    // 位置过零点处理
+    if (getTargetState().position - getState().position > 180.0f)
+        getTargetState().position -= 360.0f;
+    else if (getTargetState().position - getState().position < -180.0f)
+        getTargetState().position += 360.0f;
+    // 计算控制量
+    refState.position = getTargetState().position;
+    if (angleLoop != nullptr) {
+        refState.velocity =
+            getTargetState().velocity +
+            angleLoop->calculate(refState.position, state.position);
+    } else {
+        refState.velocity = getTargetState().velocity;
+    }
+    if (speedLoop != nullptr) {
+        refState.toreque =
+            getTargetState().toreque +
+            speedLoop->calculate(refState.velocity, state.velocity);
+    } else {
+        refState.toreque = getTargetState().toreque;
+    }
+
+    return refState.toreque;
+}
+
+uint32_t UnitreeA1::crc32_core(uint32_t *ptr, uint32_t len)
+{
+    uint32_t xbit = 0;
+    uint32_t data = 0;
+    uint32_t CRC32 = 0xFFFFFFFF;
+    const uint32_t dwPolynomial = 0x04c11db7;
+    for (uint32_t i = 0; i < len; i++) {
+        xbit = 1 << 31;
+        data = ptr[i];
+        for (uint32_t bits = 0; bits < 32; bits++) {
+            if (CRC32 & 0x80000000) {
+                CRC32 <<= 1;
+                CRC32 ^= dwPolynomial;
+            } else
+                CRC32 <<= 1;
+            if (data & xbit)
+                CRC32 ^= dwPolynomial;
+            xbit >>= 1;
+        }
+    }
+    return CRC32;
+}
---
+++ include/BaseControl/Motor/UnitreeA1.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Motor.hpp"
+#include "UnitreeA1protocol.hpp"
+
+class UnitreeA1 : public Motor {
+public:
+    UnitreeA1(Connectivity &connectivity, uint16_t send_id, uint16_t receive_id,
+              int8_t cw);
+    ~UnitreeA1();
+
+    UnitreeA1 &init() final;
+    UnitreeA1 &deInit() final;
+
+    UnitreeA1 &encodeControlMessage() final;
+    UnitreeA1 &decodeFeedbackMessage() final;
+
+private:
+    uint8_t ifEnable = 0;
+
+    float calculateControlData() final;
+
+    uint32_t crc32_core(uint32_t *ptr, uint32_t len);
+};
