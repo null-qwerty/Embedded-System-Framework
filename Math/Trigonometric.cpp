f5050ebd5031ee8acc25b912bdd1d9cb9f43b53a
Haoyu Chen <null-qwerty@outlook.com>
2025-03-31 18:23:02 +0800

    fix(Math): simply add namespace to avoid confliction with std math function

--- Math/Trigonometric.cpp
+++ Math/Trigonometric.cpp
@@ -2,6 +2,8 @@
 #include "arm_math.h"
 #include "arm_math_types.h"
 
+namespace Math
+{
 float32_t sin(float32_t x)
 {
     return arm_sin_f32(x);
@@ -17,4 +19,5 @@ float32_t atan2(float32_t y, float32_t x)
     float32_t res;
     arm_atan2_f32(y, x, &res);
     return res;
+}
 }
